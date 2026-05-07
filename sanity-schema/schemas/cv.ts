85b82aa7de3ca75ada0d006b4ad65fe1f7731d0e
dmc0125 <dominikmichal0124@gmail.com>
2026-05-07 22:51:27 +0200

    astro

--- sanity-schema/schemas/cv.ts
+++
@@ -1,5 +0,0 @@
-export default {
-	name: 'cv',
-	type: 'document',
-	fields: [{ name: 'cv', type: 'file' }],
-};
--- sanity-schema/schemas/index.ts
+++
@@ -1,4 +0,0 @@
-import cv from './cv';
-import project from './project';
-
-export const schemaTypes = [project, cv];
